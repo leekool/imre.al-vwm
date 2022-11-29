d2a3acbdfab6c5837328c26d50caadaf2872c7f5
leekool <leejedda@gmail.com>
2022-11-30 00:06:43 +1100

    navbar changed to window

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { WindowComponent } from './window/window.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    WindowComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/window/window.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'window',
+  templateUrl: './window.component.html',
+  styleUrls: ['./window.component.css']
+})
+
+export class WindowComponent {
+  @Input() title: string = '';
+
+  constructor() { }
+}
