0f69f6e2a0475b6d45e2dc8ce142dcc946105a0d
leekool <leejedda@gmail.com>
2022-12-14 22:17:05 +1100

    window class

--- src/app/desktop/desktop.component.ts
+++ src/app/desktop/desktop.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { WindowService } from '../window.service';
+import { Window } from '../window/window'
 
 @Component({
   selector: 'desktop',
   templateUrl: './desktop.component.html',
   styleUrls: ['./desktop.component.css']
 })
+
 export class DesktopComponent implements OnInit {
-  windowList: any
+  windowList: any;
+
+  desktopIcons = [{
+    title: 'imre.al',
+    highlight: false,
+  }]
+
+  imreal: Window = new Window('imre.al');
 
   constructor(public windowService: WindowService) { }
 
--- src/app/window/window.ts
+++ src/app/window/window.ts
@@ -1,4 +1,21 @@
-export interface Window {
-    title: string;
-    icon: string;
+export class Window {
+    title: string = '';
+    highlight: boolean = false;
+    desktopIcon: string = '';
+
+    constructor(title: string) {
+        this.title = title;
+        this.setDesktopIcon();
+    }
+
+    setDesktopIcon() {
+        this.desktopIcon = this.highlight
+            ? `assets/icons/${this.title}-desktop-icon-highlight.png`
+            : `assets/icons/${this.title}-desktop-icon.png`;
+    }
+
+    toggleHighlight() {
+        this.highlight = !this.highlight;
+        this.setDesktopIcon();
+    }
 }
