85245834d7fb2edcc161feb7b7d7efa3ac548b95
leekool <leejedda@gmail.com>
2023-01-07 18:56:25 +1100

    remove ascii art

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -6,7 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent {
-  @Input() content: string = '   _/_/_/    _/_/_/_/    _/_/    _/\n   _/    _/  _/        _/    _/  _/\n  _/_/_/    _/_/_/    _/_/_/_/  _/\n _/    _/  _/        _/    _/  _/\n_/    _/  _/_/_/_/  _/    _/  _/_/_/_/';
+  @Input() content: string = '';
   @Input() title: string = '';
 
   lineCount(): number[] {
