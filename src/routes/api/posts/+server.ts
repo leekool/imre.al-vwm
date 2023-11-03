54530dec7abaf0e70540d7e963aa9742ad1483d7
leekool <lee@imre.al>
2023-11-04 00:14:10 +1100

    remove utils

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,7 +1,22 @@
-import { fetchMarkdownPosts } from "$lib/utils";
 import { json } from "@sveltejs/kit";
 
 export const prerender = true;
+
+const fetchMarkdownPosts = async () => {
+    const allPostFiles = import.meta.glob("/src/routes/post/*.md");
+    const iterablePostFiles = Object.entries(allPostFiles);
+
+    const allPosts = await Promise.all(
+        iterablePostFiles.map(async ([path, resolver]) => {
+            const { metadata }: any = await resolver();
+            const postPath = path.slice(11, -3); // remove "/src/routes"
+
+            return { meta: metadata, path: postPath };
+        })
+    );
+
+    return allPosts;
+}
 
 export const GET = async () => {
     const allPosts = await fetchMarkdownPosts();
